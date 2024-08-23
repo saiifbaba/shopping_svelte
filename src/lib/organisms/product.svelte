6107160075fd98a5bf508485dd4f9af4ba327ee7
saiifbaba <saifbaba888@gmail.com>
2024-08-23 18:24:43 +0530

    full product page done

---
+++ src/lib/organisms/product.svelte
@@ -0,0 +1,28 @@
+
+<script  lang="ts">
+  import Button from "$lib/atoms/button.svelte";
+  import { goto } from '$app/navigation';
+	import type { product } from "../../common/type";
+  export let product:product;
+</script>
+<div class="product w-4/5 border grid grid-cols-2 bg-[#2f4558] rounded-lg">
+<div class="product-image-container p-4 w-1/2">
+  <img class=" mix-blend-multiply" src={product.image} alt="id">
+</div>
+  <div class="flex flex-col product-details p-4">
+    <div class="title">
+      <h1 class="text-xl text-[#afb9c2] font-bold">{product.title}</h1>
+    </div>
+    <div class="product-desc font-sm  text-[#afb9c2] text-sm mt-2">
+     <p>{product.description}</p>
+    </div>
+    <div class="product-rating flex-grow flex justify-between mt-4">
+      <p class="text-[#afb9c2] heading-text-color">Rating: {product.rating.rate}</p>
+      <p class="text-[#afb9c2] ml-4 heading-text-color">Price: {`\u20B9`} {product.price}</p>
+    </div>
+    <div class="buttons-container flex w-full">
+      <Button classes="w-1/2 border p-2 bg-[#2E63D7ff] hover:text-white " label={"Back"} onClick={()=>{goto("/")}}></Button>
+      <Button classes="w-1/2 p-2 border bg-[#06D001] hover:text-white" label={"Add to Cart"} onClick={()=>{goto("/cart")}}></Button>
+    </div>
+  </div>
+</div>
