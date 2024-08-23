16445ea7772d99046ced59b6d1335233b1c063d5
saiifbaba <saifbaba888@gmail.com>
2024-08-23 18:26:56 +0530

    search feature added

---
+++ src/lib/atoms/input/input.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+export let classes="";
+export let placeholder:string;
+export let onSearch:(search:string)=>void;
+</script>
+
+
+<div class="input">
+<input placeholder={placeholder} on:input={(e)=>{onSearch(e.target?.value)}} class={classes}>
+</div>
