0e99e66289f98b75d635bd5d97ae3d7ba776ceba
saiifbaba <saifbaba888@gmail.com>
2024-08-23 20:53:54 +0530

    modifications for container testcase done

--- src/lib/molecules/card-container/card-container.test.ts
+++ src/lib/molecules/card-container/card-container.test.ts
@@ -2,6 +2,9 @@ import { expect, test, vi } from 'vitest';
 import { render, screen } from '@testing-library/svelte';
 import CardContainer from './card-container.svelte'; // Assuming your component's path
 import { SelectOption, type product } from '../../common/type';
+import { currentPage } from './card-container.svelte';
+import { maxPages } from './card-container.svelte';
+import { selectedCategories } from './card-container.svelte';
 
 const mockData: product[] = [
   {
@@ -9,7 +12,7 @@ const mockData: product[] = [
 "title": "Product 1",
 "price": 100,
 "description": "Product 1 desc",
-"category": "category",
+"category": "electronics",
 "image": "img",
 "rating": {
 "rate": 2,
@@ -21,7 +24,7 @@ const mockData: product[] = [
   "title": "Product 2",
   "price": 200,
   "description": "Product 2 desc",
-  "category": "category",
+  "category": "women's clothing",
   "image": "img",
   "rating": {
   "rate": 4,
@@ -30,8 +33,31 @@ const mockData: product[] = [
 }
 ];
 
+const updateMaxPages = vi.fn((max) => {
+});
+const updateCurrentPage=vi.fn((page)=>{
+})
+const mockRating=5;
+const mockLimit=5;
+const mockPrice=400;
+const mockMaxPages=4;
+const mockCurrentPage=1;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 test('renders Product List with default props', () => {
-  render(CardContainer, { data: mockData,selected:SelectOption.Price });
+  render(CardContainer, { data: mockData,selected:SelectOption.Price,updateCurrentPage,updateMaxPages,rating:mockRating,price:mockPrice,limit:mockLimit,currentPage:mockCurrentPage,maxPages:mockMaxPages});
   // Verify product cards are rendered
   const cardContainer = screen.getByTestId('card-container-element'); 
   expect(cardContainer).toBeInTheDocument();
@@ -42,7 +68,7 @@ test('renders Product List with default props', () => {
 });
 
 test('sorts products by price when selected option is Price', () => {
-  render(CardContainer, { data: mockData, selected: SelectOption.Price });
+  render(CardContainer, { data: mockData, selected: SelectOption.Price,updateCurrentPage,updateMaxPages,rating:mockRating,price:mockPrice,limit:mockLimit,currentPage:mockCurrentPage,maxPages:mockMaxPages});
   const cardList = screen.getAllByRole('img');
   let LowestPriceCard:product=mockData[0];  
 
@@ -53,7 +79,7 @@ test('sorts products by price when selected option is Price', () => {
 
 test('sorts products by highest rating when selected option is Rating', () => {
   let HighestRatingCard:product=mockData[1];  
-  render(CardContainer, { data: mockData, selected: SelectOption.Rating });
+render(CardContainer, { data: mockData, selected: SelectOption.Rating,updateCurrentPage,updateMaxPages,rating:mockRating,price:mockPrice,limit:mockLimit,currentPage:mockCurrentPage,maxPages:mockMaxPages });
   const cardList = screen.getAllByRole('img');
   console.log(mockData);
   expect(cardList[0].id).toBe(HighestRatingCard.id); 
@@ -62,7 +88,41 @@ test('sorts products by highest rating when selected option is Rating', () => {
 
 test('filters products by search text', () => {
   let SearchedCard:product=mockData[1];  
-  render(CardContainer, { data: mockData, searchText: 'Product 1' });
+  render(CardContainer, { data: mockData, searchText: 'Product 1',updateCurrentPage,updateMaxPages,rating:mockRating,price:mockPrice,limit:mockLimit,currentPage:mockCurrentPage,maxPages:mockMaxPages });
+  const cardList = screen.getAllByRole('img'); 
+  expect(cardList.length).toBe(1);
+  expect(cardList[0].id).toBe(SearchedCard.id);
+});
+
+test('filters products on based of category', () => {
+  let category="women's clothing";
+  let SearchedCard:product=mockData.filter((data)=>{
+    return data.category===category;
+  })[0];  
+  render(CardContainer, { data: mockData,selectedCategories:[category], updateCurrentPage,updateMaxPages,rating:mockRating,price:mockPrice,limit:mockLimit,currentPage:mockCurrentPage,maxPages:mockMaxPages });
+  const cardList = screen.getAllByRole('img'); 
+  expect(cardList.length).toBe(1);
+  expect(cardList[0].id).toBe(SearchedCard.id);
+});
+
+
+  test('filters products on based of top rating', () => {
+  let rating =2;
+  let SearchedCard:product=mockData.filter((data)=>{
+    return data.rating.rate<=rating
+  })[0];  
+  render(CardContainer, { data: mockData, updateCurrentPage,updateMaxPages,rating,price:mockPrice,limit:mockLimit,currentPage:mockCurrentPage,maxPages:mockMaxPages });
+  const cardList = screen.getAllByRole('img'); 
+  expect(cardList.length).toBe(1);
+  expect(cardList[0].id).toBe(SearchedCard.id);
+});
+
+test('filters products based on low to high price', () => {
+  let priceRange=150;
+  let SearchedCard:product=mockData.filter((data)=>{
+    return data.price<=priceRange
+  })[0];  
+  render(CardContainer, { data: mockData, updateCurrentPage,updateMaxPages,rating:mockRating,price:priceRange,limit:mockLimit,currentPage:mockCurrentPage,maxPages:mockMaxPages });
   const cardList = screen.getAllByRole('img'); 
   expect(cardList.length).toBe(1);
   expect(cardList[0].id).toBe(SearchedCard.id);
