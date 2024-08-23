0eaa43bc364201c34aa760bfad5682058bf1f74f
saiifbaba <saifbaba888@gmail.com>
2024-08-23 21:06:03 +0530

    error page added

--- src/lib/atoms/star/star.svelte
+++ src/lib/atoms/star/star.svelte
@@ -5,11 +5,15 @@
   $:unfilled=5-rating;
 </script>
 
-<div class="flex">
-{#each Array(rating) as _, index}
-<FilledStar/>
-{/each}
-{#each Array(unfilled) as _, index}
-<UnfilledStar/>
-{/each}
+<div data-testid="star-test" class="flex">
+  <span data-testid="filled-stars" class="flex">
+    {#each Array(rating) as _, index}
+    <FilledStar/>
+    {/each}
+  </span>
+  <span data-testid="unfilled-stars" class="flex">
+    {#each Array(unfilled) as _, index}
+    <UnfilledStar/>
+    {/each}
+  </span>
 </div>
---
+++ src/routes/+error.svelte
@@ -0,0 +1,9 @@
+<script>
+	import { goto } from '$app/navigation';
+	import { page } from '$app/stores';
+	import Button from '$lib/atoms/button/button.svelte';
+</script>
+
+<div class="ml-2 flex gap-2 mt-2">
+  <a href="/" class="font-bold text-xl">Page Not Found. Go Back To Main Page</a>
+</div>
