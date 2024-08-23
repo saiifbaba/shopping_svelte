1a0b1507f51fed81eb457ce244e0e1d15573098b
saiifbaba <saifbaba888@gmail.com>
2024-08-23 17:10:01 +0530

    created button compo

---
+++ src/lib/atoms/button/button.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+  export let onClick:()=>void;
+  export let label:string;
+  export let disabled:boolean=false;
+  export let classes:string="";
+</script>
+
+
+<button disabled={disabled} on:click={onClick} class={classes}>{label}
+  <slot/>
+</button>
+
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,3 +3,21 @@
 </script>
 
 <slot />
+
+
+
+
+
+
+
+
+
+
+<div class="main-component h-screen flex flex-col">
+	<div class="layout"> 
+		<Header></Header>
+	</div>
+	<div class="flex-1 h-full">
+		<slot></slot> 
+		</div>
+	</div>
