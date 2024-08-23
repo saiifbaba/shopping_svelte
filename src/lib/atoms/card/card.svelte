24db7bf2a8db1775114a142f219016614838ec0f
saiifbaba <saifbaba888@gmail.com>
2024-08-23 17:19:37 +0530

    page header

---
+++ src/lib/atoms/card/card.svelte
@@ -0,0 +1,33 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<div data-testid="card-element" on:click={()=>onClick(id)} class="card-background-color card cursor-pointer font-serif  w-full h-42 lg:w-72 lg:h-72 rounded overflow-hidden shadow-lg bg-white flex flex-col items-center">
+  <div class="w-full h-1/2 mt-2 flex justify-center">
+    <img  id={id} class="h-full  mix-blend-darken " src={imageUrl} alt="image-alt">
+  </div>
+  <div class="px-3 py-2 lg:px-6 lg:py-4 flex-grow flex items-center">
+    <p class="lg:font-bold text-sm lg:mb-2">{label.length>60?label.substring(0,60):label}</p> 
+  </div>
+  <div class="flex flex-col lg:flex-row items-center mb-2 justify-between w-full px-5">
+    <p class="font-mono">{`\u20B9`}{price}</p>
+    <slot></slot>
+  </div>
+</div>
---
+++ src/lib/organisms/header.svelte
@@ -0,0 +1,33 @@
+
+
+
+
+
+
+
+
+
+
+<div class="header w-full p-4  flex items-center justify-between bg-black">
+  <div class="title-header">
+    <h1 class="p-2 text-white font-serif font-bold text-xl lg:text-3xl">Fake Store</h1>
+  </div>
+  {#if showSearch}
+  <div class="w-1/2 flex items-center justify-around">
+    <div class="search-container w-4/5 flex bg-white rounded hover:cursor-pointer">
+      <div class="grow">
+        <Input placeholder="Search for product.."  onSearch={onSearch} classes="w-full   rounded px-2 text-slate-400 p-1 outline-none">
+        </Input>
+      </div>
+      <div>
+        <div class="bg-[#feba6a] h-full w-full flex items-center px-2 rounded">
+          <SearchIcon width="20px" height="20px"></SearchIcon>
+        </div>
+      </div>
+    </div>
+    <button class="cart-icon hover:cursor-pointer" on:click={onCartClick}>
+    <CartIcon fillColor="#feba6a"></CartIcon>
+    </button>
+    </div>
+  {/if}
+</div>
