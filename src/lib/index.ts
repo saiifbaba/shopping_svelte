0497fe4fafc9776bfeebf19bd1b5921ae51c439d
saiifbaba <saifbaba888@gmail.com>
2024-08-23 18:19:07 +0530

    coontainer added for storing

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,1 +1,5 @@
 // place files you want to import through the `$lib` alias in this folder.
+import Dashboard from "./organisms/dashboard.svelte";
+import Button from "./atoms/button/button.svelte";
+import Card from "./atoms/card/card.svelte";
+import Dropdown from "./atoms/dropdown/dropdown.svelte";
---
+++ src/lib/molecules/card-container/card-container.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+ import {SelectOption,type product } from "../../common/type";
+ import Card from "../atoms/card.svelte";
+ export let data:product[];
+ export let selected:SelectOption;
+ let filteredData:product[]=[];
+ $:if(selected!==SelectOption.Default){
+    filteredData=data.sort((item1:product,item2:product)=>{
+     return selected===SelectOption.Price?item1.price-item2.price:item2.rating.rate-item1.rating.rate;
+    })
+  }else{
+      filteredData=data;
+  }
+</script>
+
+<div class="card-container w-full lg:w-4/5  grid grid-cols-2  lg:grid-cols-3 gap-4">
+    {#each  filteredData as item}
+        <Card label={item.title} price={item.price} rating={item.rating.rate} imageUrl={item.image}></Card>
+    {/each}
+</div>
